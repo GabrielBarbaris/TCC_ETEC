131cd45120858500b1dc492aad6eceebf000c635
LucasKawe <lucas.figueira2@etec.sp.gov.br>
2025-08-05 13:54:08 -0300

    teste

---
+++ cadastroProduto.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<header class="HEADER">
+        <div class="overlap-17">
+          <div class="CATEGORIAS">
+
+            <div class="CHURRASCO">
+              <div class="overlap-group-3">
+                <img class="rectangle-7" src="img/bordaCategoria.png" />
+                <div class="text-wrapper-57">CHURRASCO</div>
+                <img class="weber" src="img/churrasco.png" />
+              </div>
+            </div>
+            <div class="KITS">
+              <div class="overlap-11">
+                <div class="text-wrapper-58">KITS</div>
+                <img class="rectangle-7" src="img/bordaCategoria.png" />
+                <img class="shopping-basket" src="img/kits.png" />
+              </div>
+            </div>
+            <div class="AVES">
+              <div class="overlap-12">
+                <img class="rectangle-7" src="img/bordaCategoria.png" />
+                <div class="text-wrapper-59">AVES</div>
+                <img class="poultry-leg" src="img/aves.png" />
+              </div>
+            </div>
+            <div class="EMBUTIDOS">
+              <div class="overlap-13">
+                <div class="text-wrapper-60">EMBUTIDOS</div>
+                <img class="rectangle-7" src="img/bordaCategoria.png" />
+                <img class="salami" src="img/embutido.png" />
+              </div>
+            </div>
+            <div class="SUNOS">
+              <div class="overlap-14">
+                <img class="rectangle-8" src="img/bordaCategoria.png" />
+                <div class="text-wrapper-61">SUiNOS</div>
+                <img class="bacon" src="img/suino.png" />
+              </div>
+            </div>
+            <div class="LINGUIAS">
+              <div class="overlap-15">
+                <img class="rectangle-7" src="img/bordaCategoria.png" />
+                <div class="text-wrapper-62">LINGUICAS</div>
+                <img class="vector-2" src="img/linguica.png" />
+              </div>
+            </div>
+            <div class="BOVINOS">
+              <div class="overlap-16">
+                <img class="rectangle-9" src="img/bordaCategoria.png" />
+                <div class="text-wrapper-63">BOVINOS</div>
+                <img class="barbecue" src="img/bovinos.png" />
+              </div>
+            </div>
+          </div>
+          <div class="rectangle-10"></div>
+          <div class="rectangle-11"></div>
+
+          <div class="CONES">
+            <div class="search">
+              <label for="searchInput">
+                <span class="material-symbols-outlined"> Search</span>
+              </label>
+              <input type="text" id="searchInput" placeholder="pesquisar">
+            </div>
+            <button onclick="cadastrar_cliente()">
+              <img class="user-user" src="img/login.png" />
+            </button>
+
+            <button>
+              <img class="basket" src="img/pedido.png" />
+            </button>
+          </div>
+          <img class="logo-2" src="img/logo.png" />
+
+
+
+        </div>
+      </header>
+</body>
+</html>
