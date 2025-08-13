1ff5186f9c55943dd3cc08aa3a2b94a3b031ec0f
LucasKawe <lucas.figueira2@etec.sp.gov.br>
2025-08-13 10:22:57 -0300

    Cadastra

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -38,19 +38,62 @@
         </div>
       </header>
       <div class="container">
-          <div class="imagem">
-            
-          </div>
-          <div class="formulario">
-            <div class="form">
-              <p> CADASTRO PRODUTO </P> 
-                 <input type="text" name="nome" required>
+    <div class="imagem">
+        <!-- aqui vai sua área de imagem -->
+    </div>
+    <div class="formulario">
+        <div class="form">
+            <p class="titulo">CADASTRO PRODUTO</p>
 
-                <input type="email" name="email" required>
-
-              <textarea name="mensagem" rows="4"></textarea>
-
+            <!-- Campo Nome -->
+            <div class="form_content">
+                <label for="nome">Nome</label>
+                <input type="text" id="nome" name="nome" placeholder="Digite o nome do produto">
+                <a>mensagem de erro</a>
             </div>
+
+            <!-- Linha com Preço e Categoria -->
+            <div class="form_row">
+                <div class="form_content">
+                    <label for="preco">Preço</label>
+                    <input type="text" id="preco" name="preco" placeholder="R$00,00">
+                    <a>mensagem de erro</a>
+                </div>
+                <div class="form_content">
+                    <label for="categoria">Categoria</label>
+                    <select id="categoria" name="categoria">
+                        <option>Escolha a categoria</option>
+                        <!-- outras opções -->
+                    </select>
+                    <a>mensagem de erro</a>
+                </div>
+            </div>
+
+            <div class="radio-group">
+                <p class="tipo">Tipo</p>
+                <label class="radio-item">
+                  <input type="checkbox" name="tipo" value="manta">
+                  <span>Manta</span>
+                </label>
+                <label class="radio-item">
+                  <input type="checkbox" name="tipo" value="bife">
+                  <span>Bife</span>
+                </label>
+                <label class="radio-item">
+                  <input type="checkbox" name="tipo" value="panela">
+                  <span>Panela</span>
+                </label>
+                <label class="radio-item">
+                  <input type="checkbox" name="tipo" value="moida">
+                  <span>Moída</span>
+                </label>
+                <!-- continue com os outros -->
+            </div>
+
+        </div>
+    </div>
+</div>
+
           </div>
       </div>
       </div>  
